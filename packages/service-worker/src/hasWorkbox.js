5640874bf834e7a78bb570e0ac3e5488de941a1c
Cansin Yildiz <cansinyildiz@gmail.com>
2020-05-11 21:21:10 +0300

    Integrate more workbox logic to the service-worker.

--- packages/service-worker/src/hasWorkbox.js
+++ packages/service-worker/src/hasWorkbox.js
@@ -1,16 +1,33 @@
+import * as googleAnalytics from "workbox-google-analytics";
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+
 import hasOffline from "./hasOffline";
 import hasShowSkipWaiting from "./hasShowSkipWaiting";
 import hasRoutes from "./hasRoutes";
 
 export default function hasWorkbox({
+  cleanupOutdatedCaches: shouldCleanupOutdatedCaches = true,
+  offlineGoogleAnalytics = false,
   offlinePath: path = "/offline",
   showSkipWaitingPrompt = false,
 } = {}) {
+  precacheAndRoute(self.__WB_MANIFEST);
+
+  if (shouldCleanupOutdatedCaches) {
+    cleanupOutdatedCaches();
+  }
+
+  if (offlineGoogleAnalytics) {
+    googleAnalytics.initialize(offlineGoogleAnalytics);
+  }
+
   if (path) {
     hasOffline({ path });
   }
+
   if (showSkipWaitingPrompt) {
     hasShowSkipWaiting();
   }
+
   hasRoutes();
 }
